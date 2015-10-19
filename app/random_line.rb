e0e3bded7d77f96ae58300c26321c3cbdd92cf0f
Jack Hall <jack@Feynman.local>
2015-10-18 23:13:54 -0500

    solutions for single and double pass

--- app/random_line.rb
+++ app/random_line.rb
@@ -1,2 +1,23 @@
 def random_line(file_path)
+  return nil unless File.exists?(file_path)
+
+  lines = IO.readlines(file_path)
+  return "" if lines.empty?
+
+  lines[Random.rand(lines.count - 1)].strip
+end
+
+def random_line_one_pass(file_path)
+  return nil unless File.exists?(file_path)
+
+  file = File.new(file_path)
+  lines = []
+  random_line = ""
+  while (line = file.gets) != nil
+    lines << line
+
+    random_line = lines[Random.rand(lines.count - 1)].strip
+  end
+
+  random_line
 end
--- spec/random_line_spec.rb
+++ spec/random_line_spec.rb
@@ -5,4 +5,38 @@ describe "random_line" do
   it "returns nil when the file doesn't exist" do
     random_line("some-random-file").should == nil
   end
+
+  it "returns \"\" when the file is empty" do
+    file_path = File.join(File.dirname(__FILE__), '../test_files/empty.txt')
+    random_line(file_path).should == ""
+  end
+
+  context "random is stubbed to 7" do
+    it "returns the seventh line of ten_lines.txt (7)" do
+      Random.stub(:rand).and_return(7)
+
+      file_path = File.join(File.dirname(__FILE__), '../test_files/ten_lines.txt')
+      random_line(file_path).should == "8"
+    end
+  end
+end
+
+describe "random_line_one_pass" do
+  it "returns nil when the file doesn't exist" do
+    random_line_one_pass("some-random-file").should == nil
+  end
+
+  it "returns \"\" when the file is empty" do
+    file_path = File.join(File.dirname(__FILE__), '../test_files/empty.txt')
+    random_line_one_pass(file_path).should == ""
+  end
+
+  context "random is stubbed to 0" do
+    it "returns the seventh line of ten_lines.txt (7)" do
+      Random.stub(:rand).and_return(0)
+
+      file_path = File.join(File.dirname(__FILE__), '../test_files/ten_lines.txt')
+      random_line_one_pass(file_path).should == "1"
+    end
+  end
 end
